78bebce0324dd0e7512f0a0b31186a30545d6f34
Ivan <ivan1501@gmail.com>
2020-03-22 14:27:43 +0100

    Social and education component completed.

---
+++ src/components/Social.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    render() {
+        return (
+            <div class="social">
+            <a href="https://github.com/ivanradunkovic" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+            <a href="https://www.facebook.com/ivan.radunkovic" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.linkedin.com/in/ivan-radunković-a2a836141/" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://stackoverflow.com/users/12302443/ivan-radunković" rel="noopener noreferrer"><i class="fab fa-stack-overflow"></i></a>
+            </div>
+            )
+        }
+    }
+    
+export default Social
