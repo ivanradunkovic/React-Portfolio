bc32a02a40d8450a55d5363a660a11cc50d9d6d7
Ivan <ivan1501@gmail.com>
2020-03-22 13:32:45 +0100

    Navbar and NavItem completed. Links inside navigations are working.

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -5,10 +5,8 @@ class Navitem extends Component {
 
 render() {
     return (
-        <li>
-            <Link to={this.props.tolink} >
-            {this.props.item}
-            </Link>
+        <li id={this.props.item}>
+            <Link to={this.props.tolink} onClick={this.props.activec.bind(this,this.props.item)}>{this.props.item}</Link>
         </li> 
         )
     }
